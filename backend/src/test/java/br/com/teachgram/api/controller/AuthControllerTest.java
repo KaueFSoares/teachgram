755f7f2b1568717ea4c67363f9c4c4f5db2310a4
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-22 23:36:37 -0300

    test: optimize imports on auth controller test

--- backend/src/test/java/br/com/teachgram/api/controller/AuthControllerTest.java
+++ backend/src/test/java/br/com/teachgram/api/controller/AuthControllerTest.java
@@ -12,21 +12,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 class AuthControllerTest extends TestsBase {
