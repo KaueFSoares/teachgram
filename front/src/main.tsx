226762370dbba6e92c701a68c033e77066fb46f7
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-24 19:46:29 -0300

    refactor: removes strict mode for turning development easier

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css"
 import "./reset.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
 )
