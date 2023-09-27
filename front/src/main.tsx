0de9e8d81037ed2738d361b092bc9ded9d04ffa8
KaueFSoares <kaue222120@gmail.com>
2023-09-27 13:56:28 -0300

    feat: sets up i18n

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,8 +1,8 @@
-import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 import "./reset.css"
+import "./i18n"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
