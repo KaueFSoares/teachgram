c18d88da42c56f1f20a02ad2be496a8387396b7c
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-27 22:08:35 -0300

    feat: creates link modal

---
+++ front/src/components/layout/form/LinkModal.tsx
@@ -0,0 +1,10 @@
+
+const LinkModal = () => {
+  return (
+    <div className="absolute w-full h-full box-border flex items-center justify-center">
+      this
+    </div>
+  )
+}
+
+export default LinkModal
