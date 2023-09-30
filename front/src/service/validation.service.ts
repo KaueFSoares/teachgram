adbf8b224b21d2a3ff52b7fdfcac05f5b359b82c
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-29 21:26:09 -0300

    refactor: changes validation logic

--- front/src/service/validation.service.ts
+++ front/src/service/validation.service.ts
@@ -1,36 +1,27 @@
 
-interface Item {
-    tag: string
-    value: string
-    email?: boolean
+interface ItemForEmptyCheck {
+    flag: boolean,
+    item: {
+      tag: string
+      value: string
+    }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function isValidEmail(email: string): boolean {
   const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
   
   return emailRegex.test(email)
 }
 
-
-export const validate = (items: Item[]) => {
-  for (const item of items) {
-    if (item.email && !isValidEmail(item.value)) {
-      return {
-        flag: false,
-        item: item,
-      }
-    }
-
-    if (item.value.length <= 0) {
-      return {
-        flag: false,
-        item: item,
-      }
+export const emptyValidation = (items: ItemForEmptyCheck[]) => {
+  for (const forItem of items) {
+    if (forItem.item.value.length <= 0) {
+      forItem.flag = false
+    } else {
+      forItem.flag = true
     }
   }
 
-  return {
-    flag: true,
-    item: {} as Item,
-  }
+  return items
 }
