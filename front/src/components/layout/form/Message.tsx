59a65627b423fba7be6bbda824e2ede17bffa544
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-29 22:15:27 -0300

    refactor: changes message text aligment

--- front/src/components/layout/form/Message.tsx
+++ front/src/components/layout/form/Message.tsx
@@ -7,7 +7,7 @@ const Message = ({ text } : MessageProps) => {
   return (
     <div className="w-full flex justify-end items-center gap-2 ">
       <img src="/icon/ball.svg" alt="" />
-      <p className="text-orange font-semibold text-lg">
+      <p className="text-orange font-semibold text-lg text-center">
         {text}
       </p>
     </div>
