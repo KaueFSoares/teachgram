7f7c236fb6a6bf67d68be1624aef738ce4bbb83e
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-30 17:43:50 -0300

    feat: creates post items

---
+++ front/src/components/layout/util/PostItem.tsx
@@ -0,0 +1,52 @@
+import { Post } from "../../../interface/home/Post"
+import { getTimeAgo } from "../../../util/getTimeAgo"
+
+interface PostItemProps {
+  post: Post
+}
+
+const PostItem = ({ post }: PostItemProps) => {
+  return (
+    <div className="w-full shadow-full p-4 rounded-lg flex flex-col gap-4">
+      <header className="w-full flex gap-4">
+        <div className="w-1/6 aspect-square overflow-hidden flex items-center justify-center rounded-full ">
+          <img src={post.userPhotoLink} alt="" className="w-full" />
+        </div>
+
+        <div className="flex justify-between flex-grow text-gray">
+          <div className="flex flex-col">
+            <h1 className="text-base">{post.username}</h1>
+            <h2 className="text-sm">há {getTimeAgo(post.createdAt).value} {getTimeAgo(post.createdAt).unit}</h2>
+          </div>
+
+          <button className="h-full flex w-4 justify-end">
+            <img src="/icon/options.svg" alt="" className="h-1/3" />
+          </button>
+        </div>
+      </header>
+
+      <main className="w-full flex flex-col gap-4">
+
+        {post.description && (
+          <p className="text-gray text-base">
+            {post.description}
+          </p>
+        )}
+
+        {post.photoLink && (
+          <img src={post.photoLink} alt="" className="w-full rounded-xl" />
+        )}
+
+      </main>
+
+      <footer className="w-full flex items-center text-gray text-sm">
+        <button className="flex gap-4">
+          <img src="/icon/outline_heart.svg" alt="" className="" />
+          <p>{post.likes} curtidas</p>
+        </button>
+      </footer>
+    </div>
+  )
+}
+
+export default PostItem
--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -1,12 +1,44 @@
 import { useTranslation } from "react-i18next"
 import Navbar from "../components/layout/navbar/Navbar.tsx"
+import { Post } from "../interface/home/Post.ts"
+import PostItem from "../components/layout/util/PostItem.tsx"
 
-const HomePage = () => {
+interface HomePageProps {
+  posts: Post[]
+  incrementPage: () => void
+}
+
+const HomePage = ({ posts, incrementPage }: HomePageProps) => {
   const { t } = useTranslation()
   
   return (
-    <main className="relative w-full h-screen flex items-center justify-center">
-      {t("homepage.test")}
+    <main className="relative w-full min-h-screen flex items-center justify-start flex-col">
+      <div className="w-full flex flex-col h-full p-12 gap-8 mb-10">
+        <img src="/images/full_logo.svg" alt="" className="w-2/3 mb-4" />
+        {posts.length > 0 ? (
+          <>
+            {posts.map((post) => (
+              
+              <PostItem  key={post.id} post={post} />
+
+            ))}
+          </>
+        ) : (
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-gray-800">
+              Nenhum post encontrado
+            </h1>
+          </div>
+        )}
+
+        {/* remove */}
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          onClick={incrementPage}
+        >
+          {t("Carregar mais")}
+        </button>
+      </div>
       <Navbar />
     </main>
   )
