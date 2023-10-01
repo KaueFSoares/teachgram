c876feddef48bc97fb44b950d35138e55f89de77
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-10-01 11:32:52 -0300

    feat: consumes navbar context on navbar

--- front/src/components/navbar/Navbar.tsx
+++ front/src/components/navbar/Navbar.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { useTranslation } from "react-i18next"
 import { useUser } from "../../service/user.service.ts"
+import NavbarContext from "../../context/NavbarContext.ts"
 import NavbarItem from "./NavbarItem.tsx"
 
 
 const Navbar = () => {
   const [ imgSrc, setImgSrc ] = useState<string>("")
+
+  const { setShowFriendsModal, setShowNewPostModal, setShowSettingsModal } = useContext(NavbarContext)
 
   const { t } = useTranslation()
 
@@ -29,36 +32,51 @@ const Navbar = () => {
                     lg:flex"/>
       <ul className="w-full flex justify-between
                     lg:flex-col lg:gap-8">
+
         <NavbarItem order={"order-1"}>
           <Link className="flex items-center justify-center gap-4  lg:py-3 lg:px-4 lg:justify-start w-full " to={"/"}>
             <img src="/icon/navbar/home.svg" className="w-5 h-5" alt="" />
             <span className="hidden lg:flex text-gray text-base">{t("navbar.feed")}</span>
           </Link>
         </NavbarItem>
+
         <NavbarItem order={"order-2"}>
-          <Link className="flex items-center justify-center gap-4  lg:py-3 lg:px-4 lg:justify-start w-full " to={"/friends"}>
+          <div 
+            className="flex items-center justify-center gap-4  lg:py-3 lg:px-4 lg:justify-start w-full "
+            onClick={() => setShowFriendsModal(true)}
+          >
             <img src="/icon/navbar/friends.svg" className="w-7 h-7" alt="" />
             <span className="hidden lg:flex text-gray text-base">{t("navbar.friends")}</span>
-          </Link>
+          </div>
         </NavbarItem>
+
         <NavbarItem order={"order-3 lg:order-5"}>
-          <Link className="flex items-center justify-center gap-4  lg:py-3 lg:px-4 lg:justify-start w-full " to={"/home"}>
+          <div 
+            className="flex items-center justify-center gap-4  lg:py-3 lg:px-4 lg:justify-start w-full "
+            onClick={() => setShowNewPostModal(true)}
+          >
             <img src="/icon/navbar/new.svg" className="w-8 h-8 lg:w-6 lg:h-6" alt="" />
             <span className="hidden lg:flex text-gray text-base">{t("navbar.create")}</span>
-          </Link>
+          </div>
         </NavbarItem>
+
         <NavbarItem order={"order-4"}>
-          <Link className="flex items-center justify-center gap-4  lg:py-3 lg:px-4 lg:justify-start w-full" to={"/home"}>
+          <div 
+            className="flex items-center justify-center gap-4  lg:py-3 lg:px-4 lg:justify-start w-full"
+            onClick={() => setShowSettingsModal(true)}
+          >
             <img src="/icon/navbar/config.svg" className="w-6 h-6 lg:w-6" alt="" />
             <span className="hidden lg:flex text-gray text-base">{t("navbar.config")}</span>
-          </Link>
+          </div>
         </NavbarItem>
+
         <NavbarItem order={"order-5 lg:order-3"}>
-          <Link className="flex items-center justify-center gap-4  lg:py-3 lg:px-4 lg:justify-start w-full overflow-hidden" to={"/home"}>
+          <Link className="flex items-center justify-center gap-4  lg:py-3 lg:px-4 lg:justify-start w-full overflow-hidden" to={"/profile"}>
             <img src={imgSrc} className="w-7 h-7  rounded-full border border-gray border-solid" alt="" />
             <span className="hidden lg:flex text-gray text-base">{t("navbar.profile")}</span>
           </Link>
         </NavbarItem>
+
       </ul>
     </nav>
   )
