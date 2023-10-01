ff07554f02c012e67a1557bff538616ac49d1d60
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-30 21:43:35 -0300

    refactor: adds posts and time between posts on posts seeder

--- backend/src/main/java/br/com/teachgram/api/domain/post/PostSeeder.java
+++ backend/src/main/java/br/com/teachgram/api/domain/post/PostSeeder.java
@@ -79,11 +79,78 @@ public class PostSeeder {
                     julia
             );
 
+            var post6 = new Post(
+                    new CreatePostRequestDTO(
+                            "Post do joao2",
+                            "Look at this beautiful view",
+                            "https://images.pexels.com/photos/5968894/pexels-photo-5968894.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                            ""
+                    ),
+                    joao
+            );
+
+            var post7 = new Post(
+                    new CreatePostRequestDTO(
+                            "Post do joao3",
+                            "Look at this beautiful view",
+                            "https://images.pexels.com/photos/5968894/pexels-photo-5968894.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                            ""
+                    ),
+                    joao
+            );
+
+            var post8 = new Post(
+                    new CreatePostRequestDTO(
+                            "Post do joao3",
+                            "Look at this beautiful view",
+                            "https://images.pexels.com/photos/5968894/pexels-photo-5968894.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                            ""
+                    ),
+                    joao
+            );
+
+            var post9 = new Post(
+                    new CreatePostRequestDTO(
+                            "Post do joao4",
+                            "Look at this beautiful view",
+                            "https://images.pexels.com/photos/5968894/pexels-photo-5968894.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                            ""
+                    ),
+                    joao
+            );
+
+            var post10 = new Post(
+                    new CreatePostRequestDTO(
+                            "Post do joao5",
+                            "Look at this beautiful view",
+                            "https://images.pexels.com/photos/5968894/pexels-photo-5968894.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                            ""
+                    ),
+                    joao
+            );
+
+            post1.setCreatedAt(post1.getCreatedAt().plusMinutes(1));
+            post2.setCreatedAt(post2.getCreatedAt().plusMinutes(2));
+            post3.setCreatedAt(post3.getCreatedAt().plusMinutes(3));
+            post4.setCreatedAt(post4.getCreatedAt().plusMinutes(4));
+            post5.setCreatedAt(post5.getCreatedAt().plusMinutes(5));
+            post6.setCreatedAt(post6.getCreatedAt().plusMinutes(6));
+            post7.setCreatedAt(post7.getCreatedAt().plusMinutes(7));
+            post8.setCreatedAt(post8.getCreatedAt().plusMinutes(8));
+            post9.setCreatedAt(post9.getCreatedAt().plusMinutes(9));
+            post10.setCreatedAt(post10.getCreatedAt().plusMinutes(10));
+            
+
             postRepository.save(post1);
             postRepository.save(post2);
             postRepository.save(post3);
             postRepository.save(post4);
             postRepository.save(post5);
+            postRepository.save(post6);
+            postRepository.save(post7);
+            postRepository.save(post8);
+            postRepository.save(post9);
+            postRepository.save(post10);
 
             System.out.println("-----PostSeeder: posts created-----");
         } else {
