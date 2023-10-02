d500c2b083f5920248fe03f55befed90c4cfacc5
KaueFSoares <kaue222120@gmail.com>
2023-10-02 14:17:53 -0300

    refactor: sets right image position to top 0

--- front/src/components/util/RightImage.tsx
+++ front/src/components/util/RightImage.tsx
@@ -1,7 +1,7 @@
 
 const RightImage = () => {
   return (
-    <div className="hidden absolute bg-repeated-pattern h-full right-0 bg-repeat-y bg-contain
+    <div className="hidden absolute bg-repeated-pattern h-full top-0 right-0 bg-repeat-y bg-contain
                     lg:flex lg:w-1/4
                     xl:w-1/5
                     2xl:w-1/6">
