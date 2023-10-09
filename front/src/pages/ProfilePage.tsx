bcf9e85d1e983c68e44e9817078f839d4e94c401
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-10-09 14:41:54 -0300

    fix: solves image stretching problem

--- front/src/pages/ProfilePage.tsx
+++ front/src/pages/ProfilePage.tsx
@@ -37,85 +37,83 @@ const ProfilePage = ({ incrementPage, userPostsData, userProfileData }: ProfileP
   }, [  ])
 
   return (
-    <>
-      <main className="relative w-full min-h-screen flex items-center justify-start flex-col lg:flex-row lg:pl-[30vw] pb-4 lg:items-start">
-        <div className="flex flex-col gap-4                      
+    <main className="relative w-full min-h-screen flex items-center justify-start flex-col lg:flex-row lg:pl-[30vw] pb-4 lg:items-start">
+      <div className="flex flex-col gap-4                      
                         lg:w-3/4 lg:pt-12
                         2xl:w-3/5">
-          <header className="w-full flex p-6 justify-between items-center lg:hidden">
-            <button 
-              className="flex items-center justify-center"
-              onClick={() => navigate(-1)}
-            >
-              <img src="/icon/left_arrow.svg" alt="" className="w-5" />
-            </button>
+        <header className="w-full flex p-6 justify-between items-center lg:hidden">
+          <button 
+            className="flex items-center justify-center"
+            onClick={() => navigate(-1)}
+          >
+            <img src="/icon/left_arrow.svg" alt="" className="w-5" />
+          </button>
 
-            <button 
-              className="flex items-center justify-center"
-              onClick={() => setShowSettingsModal(true)}
-            >
-              <img src="/icon/navbar/config.svg" alt="" className="w-5" />
-            </button>
-          </header>
+          <button 
+            className="flex items-center justify-center"
+            onClick={() => setShowSettingsModal(true)}
+          >
+            <img src="/icon/navbar/config.svg" alt="" className="w-5" />
+          </button>
+        </header>
 
-          <section className="flex flex-col items-center justify-start gap-4
+        <section className="flex flex-col items-center justify-start gap-4
                               lg:flex-row">
-            <div className="w-2/5 overflow-hidden aspect-square flex items-center justify-center rounded-full mb-4
+          <div className="w-2/5 overflow-hidden aspect-square flex items-center justify-center rounded-full mb-4
                             lg:w-1/3">
-              <img src={userProfileData.photo} alt="" className="w-full" />
-            </div>
+            <img src={userProfileData.photo} alt="" className="w-full  min-h-full object-cover" />
+          </div>
 
-            <div className="flex gap-4 flex-col lg:ml-8 lg:items-start">
-              <h1 className="text-xl font-semibold">
-                {userProfileData.name}
-              </h1>
+          <div className="flex gap-4 flex-col lg:ml-8 lg:items-start">
+            <h1 className="text-xl font-semibold">
+              {userProfileData.name}
+            </h1>
 
-              <p className="text-base text-center text-gray mb-4">
-                {userProfileData.bio}
-              </p>
-            </div>
-          </section>
+            <p className="text-base text-center text-gray mb-4">
+              {userProfileData.bio}
+            </p>
+          </div>
+        </section>
 
-          <section className="w-1/2 flex items-center self-center justify-center mb-4">
-            <div className="flex-grow flex items-center justify-center flex-col">
-              <span className="text-base font-bold">
-                {userProfileData.postsNumber}
-              </span>
-              <span className="text-base text-gray font-medium">
+        <section className="w-1/2 flex items-center self-center justify-center mb-4">
+          <div className="flex-grow flex items-center justify-center flex-col">
+            <span className="text-base font-bold">
+              {userProfileData.postsNumber}
+            </span>
+            <span className="text-base text-gray font-medium">
               Posts
-              </span>
-            </div>
+            </span>
+          </div>
 
-            <div className="h-10 w-px bg-gray"></div>
+          <div className="h-10 w-px bg-gray"></div>
 
-            <div className="flex-grow flex items-center justify-center flex-col">
-              <span className="text-base font-bold">
-                {userProfileData.friendsNumber}
-              </span>
-              <span className="text-base text-gray font-medium">
+          <div className="flex-grow flex items-center justify-center flex-col">
+            <span className="text-base font-bold">
+              {userProfileData.friendsNumber}
+            </span>
+            <span className="text-base text-gray font-medium">
               Amigos
-              </span>
-            </div>
-          </section>
+            </span>
+          </div>
+        </section>
 
-          <section className="w-full grid grid-cols-3 gap-1 flex-grow">
-            {userPostsData.map((post) => {
-              return (
-                <div 
-                  key={post.id}
-                  className="flex items-center justify-center w-full aspect-square overflow-hidden"
-                >
-                  <img src={post.photo} alt="" className="min-w-full min-h-full object-cover" />
-                </div>
-              )
-            })}
-          </section>
-          <div id="load-more" className="opacity-0 h-px w-2"></div>
-        </div>
+        <section className="w-full grid grid-cols-3 gap-1 flex-grow">
+          {userPostsData.map((post) => {
+            return (
+              <div 
+                key={post.id}
+                className="flex items-center justify-center w-full aspect-square overflow-hidden"
+              >
+                <img src={post.photo} alt="" className="min-w-full min-h-full object-cover" />
+              </div>
+            )
+          })}
+        </section>
+        <div id="load-more" className="opacity-0 h-px w-2"></div>
+      </div>
 
-        { isDesktop && <Navbar /> }
-      </main>
-    </>
+      { isDesktop && <Navbar /> }
+    </main>
   )
 }
 
