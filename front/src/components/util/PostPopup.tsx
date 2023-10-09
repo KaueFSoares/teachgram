f185cb425be5e73b2d091638ba82cd1898f14ae3
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-10-09 16:59:02 -0300

    feat: implements update post from feed logic

--- front/src/components/util/PostPopup.tsx
+++ front/src/components/util/PostPopup.tsx
@@ -6,12 +6,13 @@ interface PostPopupProps {
 }
 
 const PostPopup = ({ id }: PostPopupProps) => {
-  const { changeShowDeletePostModal } = useContext(NavbarContext)
+  const { changeShowDeletePostModal, changeShowUpdatePostModal } = useContext(NavbarContext)
   
   return (
     <div className="bg-white shadow-full absolute -top-2 right-full py-4 px-6 rounded-xl flex flex-col gap-4">
       <button
         className="w-full text-center font-medium text-orange text-base"
+        onClick={() => changeShowUpdatePostModal(id)}
       >
         Editar
       </button>
