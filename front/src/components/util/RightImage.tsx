89eb0a26c72ecce61e64b2831fc3cccd7373a2cd
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-10-10 13:58:17 -0300

    refactor: resizes right image

--- front/src/components/util/RightImage.tsx
+++ front/src/components/util/RightImage.tsx
@@ -2,9 +2,8 @@
 const RightImage = () => {
   return (
     <div className="hidden absolute bg-repeated-pattern h-full top-0 right-0 bg-repeat-y bg-contain
-                    lg:flex lg:w-1/4
-                    xl:w-1/5
-                    2xl:w-1/6">
+                    lg:flex lg:w-1/6
+                    xl:w-[14%]">
       
     </div>
   )
