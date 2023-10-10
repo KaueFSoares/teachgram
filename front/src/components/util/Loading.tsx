de79f8ceec2734de9e94000b1f9c5c502b357653
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-10-10 12:26:31 -0300

    feat: adds loading logic to signup

--- front/src/components/util/Loading.tsx
+++ front/src/components/util/Loading.tsx
@@ -4,7 +4,7 @@ const Loading = () => {
   const { t } = useTranslation()
 
   return (
-    <main className="bg-orange w-full h-screen flex items-center justify-center flex-col gap-8">
+    <main className="bg-orange w-full h-screen flex items-center justify-center flex-col gap-8 absolute top-0 left-0">
       <img src="/images/short_logo.svg" alt="" className="drop-shadow-md" />
       <p className="text-white font-bold text-xl">
         {t("loading")}
--- front/src/container/SignupPageContainer.tsx
+++ front/src/container/SignupPageContainer.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 import AuthContext from "../context/AuthContext"
 import { SignupPage } from "../pages"
 import { onSignup } from "../service/auth.service"
+import Loading from "../components/util/Loading"
 
 const SignupPageContainer = () => {
   const navigate = useNavigate()
@@ -17,32 +18,44 @@ const SignupPageContainer = () => {
   const [ password, setPassword ] = useState("")
   const [ photo, setPhoto ] = useState("")
 
+  const [ loading, setLoading ] = useState(false)
+
   const handleSubmit = () => {
+    setLoading(true)
+
     onSignup(name, email, username, bio, phone, password, photo)
       .then(() => {
         setAuthenticated(true)
         navigate("/")
       })
+      .catch((err) => {
+        err === err
+        setLoading(false)
+      })
   }
   
   return (
-    <SignupPage
-      name={name}
-      email={email}
-      username={username}
-      bio={bio}
-      phone={phone}
-      password={password}
-      setName={setName}
-      setEmail={setEmail}
-      setUsername={setUsername}
-      setBio={setBio}
-      setPhone={setPhone}
-      setPassword={setPassword}
-      photo={photo}
-      setPhoto={setPhoto}
-      handleSubmit={handleSubmit}
-    />
+    loading ? (
+      <Loading />
+    ) : (
+      <SignupPage
+        name={name}
+        email={email}
+        username={username}
+        bio={bio}
+        phone={phone}
+        password={password}
+        setName={setName}
+        setEmail={setEmail}
+        setUsername={setUsername}
+        setBio={setBio}
+        setPhone={setPhone}
+        setPassword={setPassword}
+        photo={photo}
+        setPhoto={setPhoto}
+        handleSubmit={handleSubmit}
+      />
+    )
   )
 }
 
